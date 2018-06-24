cbb205878e87fe856c62aa5b7b698c9f1f323761
chrispykan <christineparisk@gmail.com>
2018-06-24 12:23:25 -0700

    some changes to html, created functions. Works..ish

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 console.log("It's-a me, JS!");
 
+function convert() {
+  //get temperature in farenheit from user input
+  var tempInFarenheit = document.getElementById("farenheitInput").value;
+  //calculate input to celcius
+  var tempInCelcius = (tempInFarenheit - 32) * (5 / 9);
+  //calculate to farenheight
+  var tempInKelvin = tempInCelcius + 273.15;
+  //display conversion in html
+  document.getElementById("celciusOutput").innerHTML = tempInCelcius;
+  document.getElementById("kelvinOutput").innerHTML = tempInKelvin;
+}
 
+console.log(tempInCelcius);
+console.log(tempInKelvin);
