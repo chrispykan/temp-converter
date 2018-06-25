991fe6ec4c7a6cabd2e142754baa2fdba638ded1
chrispykan <christineparisk@gmail.com>
2018-06-24 17:45:13 -0700

    to es6

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,11 @@
-console.log("It's-a me, JS!");
-
 function convert(tempInFarenheit) {
   //get temperature in farenheit from user input
   var tempInFarenheit = document.getElementById("farenheitInput").value;
   //calculate input to celcius
-  var tempInCelcius = (tempInFarenheit - 32) * (5 / 9);
+  const tempInCelcius = (tempInFarenheit - 32) * (5 / 9);
   //calculate to farenheight
-  var tempInKelvin = tempInCelcius + 273.15;
+  const tempInKelvin = tempInCelcius + 273.15;
   //display conversion in html
     document.getElementById("celciusOutput").innerHTML = tempInCelcius;
     document.getElementById("kelvinOutput").innerHTML = tempInKelvin;
 };
-    
